93b3a8388edc92ad3e44d6c0f4e7a8f332932e7a
Gencehan <gencehangurcam@gmail.com>
2021-10-05 14:19:52 +0200

    project init

---
+++ js/main.js
@@ -0,0 +1,56 @@
+
+
+// for (let i = 0; i <= 20; i++) {
+//     console.log(i);
+// }
+
+
+// function Odd(value) {
+//     return (value & 1)==1;
+// }
+    
+// for(let i=1; i<=10; i++) {
+//     console.log(i+" est "+(Odd(i)?"Impaire":"Pair"));
+// }
+
+
+//exo2
+
+// let array = ['Martin', 'constantin', 'billy', 'fitchie', 'Brandon', 'Corantin'];
+
+// for (let i=array.length; i--;) {
+//     array[i] = 'Bonjour ' + array[i];
+//     console.log(array[i]);
+// }
+// for (let i = 0; i < array.length; i++) {
+//     array[i] = 'Bonjour ' + array[i];
+//     console.log(array[i]);
+// }
+
+//exo3
+// let pr = Number(prompt('donne un no'))
+// console.log(pr);
+
+// for (let i = 0; i < pr.length; i++) {
+//     pr[i] = array[i];
+//     console.log(pr[i]); 
+// }
+
+
+//foreach
+//exo1
+// const array1 = ['a', 'b', 'c'];
+
+// array1.forEach(element => console.log(element));
+
+// expected output: "a"
+// expected output: "b"
+// expected output: "c"
+
+//exo2
+// let array = ['Martin', 'constantin', 'billy', 'fitchie', 'Brandon', 'Corantin'];
+// array.forEach(function (value, i) {
+//     console.log("salut ", value, i);
+    
+// });
+
